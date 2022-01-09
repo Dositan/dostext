b2ba9fbdf473cc935b4497390d0d0cb0aef096d1
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-01-09 21:19:10 +0600

    ✨ Replace data/config.json with settings.py

---
+++ dostext/settings.py
@@ -0,0 +1,11 @@
+# App Settings
+TITLE = "Dostext"
+RESOLUTION = "1200x700"
+
+# File Select
+FILETYPES = (
+    ('Text Files', '*.txt'),
+    ('Markdown Files', '*.md'),
+    ('Python Files', '*.py'),
+    ('All Files', '*.*')
+)
