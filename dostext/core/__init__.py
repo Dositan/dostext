e737ca153accf39b69359966520ef2f98a3d2d71
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-01-09 21:17:16 +0600

    ✅ Delete dostext/core/ directory

--- dostext/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .constants import *
-from .editor import *
-from .frame import *
-from .loader import *
-from .manager import *
--- dostext/core/constants.py
+++
@@ -1,7 +0,0 @@
-# For file management.
-FILETYPES = (
-    ('Text Files', '*.txt'),
-    ('Markdown Files', '*.md'),
-    ('Python Files', '*.py'),
-    ('All Files', '*.*')
-)
